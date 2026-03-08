850a2452796f712f3bd031bd3a9ca0b79bfbfb3a
Zanci19 <73758755+Zanci19@users.noreply.github.com>
2026-03-08 11:11:28 +0100

    test

---
+++ nameTutorial/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nameTutorial',
+  webDir: 'dist'
+};
+
+export default config;
